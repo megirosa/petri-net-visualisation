93b110dd68c5109862f1eb4c1e0d02ea1b517a7a
Róża <rszylar@pgs-soft.com>
2015-09-12 11:27:22 +0200

    changed files structure

---
+++ test.rb
@@ -0,0 +1,10 @@
+page :strona do
+  process = place "process", { name: :name, valid: :valid? }
+    done = place "done"
+
+    transition "work" do
+       input process
+       output done do
+    end
+  end
+end
