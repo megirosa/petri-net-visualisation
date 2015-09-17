cdaa051ebd0d9ad8c760f897d9f9b2aa646f7510
Róża <rszylar@pgs-soft.com>
2015-09-17 11:49:50 +0200

    returnig if no file given

--- bin/draw.rb
+++ bin/draw.rb
@@ -3,7 +3,14 @@ require 'ruby-graphviz'
 
 require_relative '../lib/interface_elements'
 
-load ARGV[1]
+file = ARGV[1]
+
+unless file
+  puts "No file given."
+  return
+end
+
+load file
 
 Shoes.app(width: 800, title: "Petri Net") do
   extend InterfaceElements
