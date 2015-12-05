e3ec6c3612da1b88daa3644c60dfcde2e76d7c0a
Róża <rszylar@pgs-soft.com>
2015-12-05 12:10:49 +0100

    fixed selecting

--- lib/interface_elements.rb
+++ lib/interface_elements.rb
@@ -48,6 +48,33 @@ module InterfaceElements
     end
   end
 
+  def draw_net
+    $net_image = image("tmp/output.png", margin: 10)
+    $net_image.click do |image|
+      if selected?
+        place_node
+      else
+        select_node
+      end
+
+      if placed?
+        $net_image.remove
+        draw_net
+      end
+    end
+  end
+
+  def draw_hover
+    @hover_circle = oval(mouse[1] - 25, mouse[2] - 25, 50, 50, 
+      fill: rgb(175, 238, 238, 0.5), 
+      stroke: rgb(175, 238, 238),
+      hidden: true)
+
+    motion do |top, left| 
+      @hover_circle.move(top - 25, left - 25)
+    end
+  end
+
   def add_place_action(name)
     drawer.add_place(name)
     update
@@ -65,43 +92,6 @@ module InterfaceElements
   def change_layout_action(layout)
     drawer.change_layout(layout)
     update
-  end
-
-  def draw_net
-    $net_image = image("tmp/output.png", margin: 10)
-    $net_image.click do |image|
-      @edited = if moving_allowed?
-        drawer.place_node(cursor_x, cursor_y, @edited)
-      else
-        drawer.select_node(cursor_x, cursor_y)
-      end
-
-      if @edited
-        $net_image.path = "tmp/output.png"
-        toggle_move
-      end
-    end
-  end
-
-  def draw_hover
-    @hover_circle = oval(mouse[1] - 25, mouse[2] - 25, 50, 50, 
-      fill: rgb(175, 238, 238, 0.5), 
-      stroke: rgb(175, 238, 238),
-      hidden: true)
-
-    motion do |top, left| 
-      @hover_circle.move(top - 25, left - 25)
-    end
-  end
-
-  def draw_button(label)
-    button(label) do
-      begin
-        yield
-      rescue
-        puts $!, $@
-      end
-    end
   end
 
   def export_action
@@ -126,12 +116,15 @@ module InterfaceElements
   private
 
   def toggle_move
-    @allow_move = !@allow_move
     @hover_circle.hidden = !@hover_circle.hidden
   end
 
-  def moving_allowed?
-    @allow_move
+  def selected?
+    !!@selected_node
+  end
+
+  def placed?
+    !!@placed_node
   end
 
   def cursor_x
@@ -144,5 +137,29 @@ module InterfaceElements
 
   def layout_algorithms_list
     %w(dot neato twopi circo fdp)
+  end
+
+  def select_node
+    @selected_node = drawer.select_node(cursor_x, cursor_y)
+    @placed_node = nil
+    toggle_move if selected?
+  end
+
+  def place_node
+    @placed_node = drawer.place_node(cursor_x, cursor_y, @selected_node)
+    if placed?
+      @selected_node = nil
+      toggle_move
+    end
+  end
+
+  def draw_button(label)
+    button(label) do
+      begin
+        yield
+      rescue
+        puts $!, $@
+      end
+    end
   end
 end
